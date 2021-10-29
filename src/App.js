9fa1d1915b48c171f1d5175db3c6e996b6ed2663
Alex Lee <dung.le@snapmart.ph>
2021-10-29 22:39:44 +0700

    save

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import * as React from "react";
+import "./App.css";
+class LikeDislike extends React.Component {
+
+  state = {
+    hasLiked: false,
+    hasDisliked: false,
+    like: 100,
+    disLike: 25
+  }
+
+  handleLike = () => {
+    if (!this.state.hasDisliked) {
+      this.setState({
+        hasLiked: !this.state.hasLiked
+      });
+    } else {
+      this.setState({
+        hasLiked: true,
+        hasDisliked: false
+      });
+    }
+  }
+
+  handleDislike = () => {
+    if (!this.state.hasLiked) {
+      this.setState({
+        hasDisliked: !this.state.hasDisliked
+      });
+    } else {
+      this.setState({
+        hasLiked: false,
+        hasDisliked: true
+      });
+    }
+  }
+
+  render() {
+    const { hasDisliked, hasLiked } = this.state;
+    const classLikeButton = `like-button ${hasLiked ? 'liked' : ''}`;
+    const classDisLikeButton = `dislike-button ${hasDisliked ? 'disliked' : ''}`;
+
+    return (
+      <div>
+        <button className={classLikeButton} onClick={this.handleLike}>
+          Like |
+          <span className="likes-counter">
+            {this.state.hasLiked ? this.state.like + 1 : this.state.like}
+          </span>
+        </button>
+        <button className={classDisLikeButton} onClick={this.handleDislike}>
+          Dislike |
+          <span className="dislikes-counter">
+            {this.state.hasDisliked ? this.state.disLike + 1 : this.state.disLike}
+          </span>
+        </button>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default LikeDislike;
